5c8cd49c77ae80bee630fff3318759a0fa0d2c0f
linyc1995 <linyichen1995@outlook.com>
2026-08-17 19:23:04 +0800

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,20 +1,22 @@
-import Document, { Head, Html, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from 'next/document'
 import siteConfig from '../../config/site.config'
 
 class MyDocument extends Document {
   render() {
     return (
-      <Html>
+      <Html lang="zh-CN">
         <Head>
-          <meta name="description" content="OneDrive Vercel Index" />
-          <link rel="icon" href="/favicon.ico" />
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
-          {siteConfig.googleFontLinks.map(link => (
-            <link key={link} rel="stylesheet" href={link} />
-          ))}
+          <meta name="description" content={siteConfig.title} />
+          <link rel="icon" href={siteConfig.icon} />
+          
+          {/* 安全判断：字体链接存在且为数组时才遍历渲染 */}
+          {Array.isArray(siteConfig.googleFontLinks) && siteConfig.googleFontLinks.length > 0 &&
+            siteConfig.googleFontLinks.map((link, i) => (
+              <link key={i} href={link} rel="stylesheet" />
+            ))
+          }
         </Head>
-        <body>
+        <body className="bg-white dark:bg-gray-900">
           <Main />
           <NextScript />
         </body>
