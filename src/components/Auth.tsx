47dff70aa2df965d57e63cd74bfab876e109d991
linyc1995 <linyichen1995@outlook.com>
2026-08-17 19:17:57 +0800

    Update Auth.tsx

--- src/components/Auth.tsx
+++ src/components/Auth.tsx
@@ -1,15 +1,12 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-
 import Image from 'next/image'
 import { useRouter } from 'next/router'
 import { FC, useState } from 'react'
-
 import { matchProtectedRoute } from '../utils/protectedRouteHandler'
 import useLocalStorage from '../utils/useLocalStorage'
 
 const Auth: FC<{ redirect: string }> = ({ redirect }) => {
   const authTokenPath = matchProtectedRoute(redirect)
-
   const router = useRouter()
   const [token, setToken] = useState('')
   const [_, setPersistedToken] = useLocalStorage(authTokenPath, '')
@@ -17,21 +14,19 @@ const Auth: FC<{ redirect: string }> = ({ redirect }) => {
   return (
     <div className="mx-auto flex max-w-sm flex-col space-y-4 md:my-10">
       <div className="mx-auto w-3/4 md:w-5/6">
-        <Image src={'/images/fabulous-wapmire-weekdays.png'} alt="authenticate" width={912} height={912} priority />
+        <Image src={'/images/fabulous-wapmire-weekdays.png'} alt="认证" width={912} height={912} priority />
       </div>
-      <div className="text-lg font-bold text-gray-900 dark:text-gray-100">{'Enter Password'}</div>
-
+      <div className="text-lg font-bold text-gray-900 dark:text-gray-100">请输入访问密码</div>
       <p className="text-sm font-medium text-gray-500">
-        {'This route (the folder itself and the files inside) is password protected. ' +
-          'If you know the password, please enter it below.'}
+        此文件夹及其内部文件已设置密码保护，如果你知道访问密码，请在下方输入。
       </p>
 
       <div className="flex items-center space-x-2">
         <input
-          className="flex-1 rounded border border-gray-600/10 p-2 font-mono focus:outline-none focus:ring focus:ring-blue-300 dark:bg-gray-600 dark:text-white dark:focus:ring-blue-700"
+          className="flex-1 rounded border border-gray-200 p-2 font-mono focus:outline-none focus:ring focus:ring-gray-300 dark:border-gray-600 dark:bg-gray-800 dark:text-white dark:focus:ring-gray-500"
           autoFocus
           type="password"
-          placeholder="************"
+          placeholder="请输入密码"
           value={token}
           onChange={e => {
             setToken(e.target.value)
@@ -44,7 +39,7 @@ const Auth: FC<{ redirect: string }> = ({ redirect }) => {
           }}
         />
         <button
-          className="rounded bg-blue-600 px-4 py-2 text-white hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-400"
+          className="rounded bg-gray-700 px-4 py-2 text-white hover:bg-gray-800 focus:outline-none focus:ring focus:ring-gray-400 dark:bg-gray-600 dark:hover:bg-gray-500"
           onClick={() => {
             setPersistedToken(token)
             router.reload()
